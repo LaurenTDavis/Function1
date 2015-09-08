f2e886d869c3b01179875187a8253fb2aa985c67
Student <student@RU-009.local>
2015-09-08 15:48:49 -0600

    functions

---
+++ main.js
@@ -0,0 +1,55 @@
+// var tripleFive = function() {
+
+// 	for ( var i = 0; i < 3; i++ ) {
+// 		console.log('Five!');
+// 	}
+// }
+
+// tripleFive();
+
+
+// var lastLetter = function(word1) {
+// 	return console.log(word1[word1.length - 1]);
+// }
+
+// lastLetter('island');
+
+// var square = function(num1) {
+// 	return console.log( num1 * num1); 
+// }
+
+// square(5);
+
+// var negate = function(num1) {
+// 	return console.log(num1 - (2 * num1)); 
+// }
+
+// negate(5);
+// negate(-8)
+
+// var toArray = function(num1, num2, num3) {
+// 	var myArray = [num1, num2, num3];
+// 	return console.log(myArray);
+// }
+// toArray(1, 2, 3);
+
+// var startsWithA = function(word1) {
+// 	if (word1.charAt(0) === 'a' || word1.charAt(0) === 'A') {
+// 		return console.log(true);
+// 	}
+// 	else {
+// 		return console.log(false);
+// 	}
+// }
+
+// startsWithA('aardvark');
+// startsWithA('bear');
+
+// var excite = function(word1) {
+// 	return console.log(word1 + '!!!');
+// }
+// excite('go');
+
+var hello = function() {
+	
+}
